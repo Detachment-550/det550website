563801538321facdb9f951f82632302d639ec303
jmessare46 <jmessare46@gmail.com>
2019-01-13 14:54:30 -0500

    Fixed configuration to work on server

--- application/config/email.php
+++ application/config/email.php
@@ -3,13 +3,13 @@
 $config['useragent']        = 'PHPMailer';              // Mail engine switcher: 'CodeIgniter' or 'PHPMailer'
 $config['protocol']         = 'smtp';                   // 'mail', 'sendmail', or 'smtp'
 $config['mailpath']         = '/usr/sbin/sendmail';
-$config['smtp_host']        = 'ssl://smtp.gmail.com';
+$config['smtp_host']        = 'smtp.gmail.com';
 $config['smtp_auth']        = true;                     // Whether to use SMTP authentication, boolean TRUE/FALSE. If this option is omited or if it is NULL, then SMTP authentication is used when both $config['smtp_user'] and $config['smtp_pass'] are non-empty strings.
 $config['smtp_user']        = 'afrotcdet550@gmail.com';
 $config['smtp_pass']        = 'silverfalcons550';
-$config['smtp_port']        = 465;
+$config['smtp_port']        = 587;
 $config['smtp_timeout']     = 30;                       // (in seconds)
-$config['smtp_crypto']      = 'ssl';                       // '' or 'tls' or 'ssl'
+$config['smtp_crypto']      = 'tls';                       // '' or 'tls' or 'ssl'
 $config['smtp_debug']       = 1;                        // PHPMailer's SMTP debug info level: 0 = off, 1 = commands, 2 = commands and data, 3 = as 2 plus connection status, 4 = low level data output.
 $config['debug_output']     = '';                       // PHPMailer's SMTP debug output: 'html', 'echo', 'error_log' or user defined function with parameter $str and $level. NULL or '' means 'echo' on CLI, 'html' otherwise.
 $config['smtp_auto_tls']    = true;                     // Whether to enable TLS encryption automatically if a server supports it, even if `smtp_crypto` is not set to 'tls'.
