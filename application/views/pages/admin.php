b63f2512f69c1410af5b0b609d500515365fa122
jmessare46 <jmessare46@gmail.com>
2019-01-26 18:55:41 -0500

    Made view more balanced on admin page

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -130,26 +130,30 @@
                 <button class="btn btn-sm btn-primary" type="submit" name="submit">Modify Cadet Info</button>
             </form>
           </div>
-        </div>
+        </div><br>
+
+          <div class="card">
+              <div class="card-body">
+                  <h5 class="card-title">Set Event Attendance</h5>
+                  <?php echo form_open('cadetevent/view'); ?>
+                  <select name="event">
+                      <?php
+                      foreach( $events as $event )
+                      {
+                          echo "<option value='" . $event['eventID'] . "'>" . $event['name'] . "</option>";
+                      }
+                      ?>
+                  </select><br><br>
+                  <button class="btn btn-sm btn-primary" type="submit">Select Event</button>
+                  </form>
+              </div>
+          </div>
       </div>
 
         <div class="col-4">
           <div class="card">  
             <div class="card-body">
               <h5 class="card-title">Additional Admin Links</h5>
-                <h6 class="card-title">Set Event Attendance</h6>
-                    <?php echo form_open('cadetevent/view'); ?>
-                        <select name="event">
-                            <?php
-                                foreach( $events as $event )
-                                {
-                                    echo "<option value='" . $event['eventID'] . "'>" . $event['name'] . "</option>";
-                                }
-                            ?>
-                        </select><br><br>
-                        <button class="btn btn-sm btn-primary" type="submit">Select Event</button>
-                </form><br><br>
-              
                 <h6>Delete an Event</h6>
                 <?php echo form_open('cadetevent/remove'); ?>
                   <select name="event">
