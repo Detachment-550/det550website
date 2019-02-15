1da81a667b455079b3cbceba7fc8597f30e1949e
jmessare46 <jmessare46@gmail.com>
2019-02-15 08:37:11 -0500

    Lowered the CDN calls from website

--- application/views/pages/securityquestion.php
+++ application/views/pages/securityquestion.php
@@ -4,10 +4,11 @@
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url("images/favicon.ico"); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     <link href='https://fonts.googleapis.com/css?family=Cabin' rel='stylesheet'>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url("js/jQuery/jquery-3.3.1.min.js"); ?>"></script>
+    <link rel="stylesheet" href="<?php echo base_url("css/bootstrap-4.3.1/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/bootstrap-4.3.1/css/bootstrap-grid.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/bootstrap-4.3.1/css/bootstrap-reboot.min.css"); ?>">
+    <script type="text/javascript" src="<?php echo base_url("css/bootstrap-4.3.1/js/bootstrap.min.js"); ?>"></script>
 
 	<title>Security Question</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
