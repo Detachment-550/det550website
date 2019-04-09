b1104284c0c7e79360e7aad18f07b09df0e4dc74
jmessare46 <jmessare46@gmail.com>
2019-04-09 11:29:14 -0400

    Added new Tiny MCE features to site

--- js/makepost.js
+++ js/makepost.js
@@ -1,8 +1,8 @@
 tinymce.init({
     selector: '#body',
-    toolbar: 'fontselect, fontsizeselect, link, forecolor backcolor numlist bullist',
-    menubar: 'file edit insert view format table tools help',
-    plugins: 'link lists',
+    toolbar: 'table tabledelete fontselect fontsizeselect link wordcount forecolor backcolor numlist bullist',
+    menubar: 'file table edit insert view format table tools help',
+    plugins: 'link table lists help wordcount',
     font_formats: 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;AkrutiKndPadmini=Akpdmi-n;Times New Roman=times new roman,times',
-    fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+    fontsize_formats: '8pt 10pt 11pt 12pt 14pt 18pt 24pt 36pt',
   });
